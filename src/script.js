0177dc5be1d1804c9ce43f3fee93a705c82ad16c
Ashi-Jain07 <ayushj0707@gmail.com>
2024-06-04 12:07:19 +0530

    Add feature to show weather data for current location of user

--- src/script.js
+++ src/script.js
@@ -465,3 +465,221 @@ search_box.addEventListener('blur', () => {
     
 // Initialize dropdown on page load
 updateRecentCitiesDropdown()
+
+//Write a function to get current location of user
+const getcity = () => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            console.log(position);
+            const { latitude, longitude } = position.coords;
+            const reverse_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=90c51f7abd19cb02e569c385f85ac9ef&units=metric`;
+            async function current_location() {
+                const response = await fetch(reverse_url);
+                const result = await response.json();
+                console.log(result);
+                const uniqueforecastdays = [];
+
+                //Use filter method to filter out five days data
+                const fiveDaysData = result.list.filter(forecast => {
+                    const forecastDate = new Date(forecast.dt_txt).getDate();
+                    if(!uniqueforecastdays.includes(forecastDate))
+                    return uniqueforecastdays.push(forecastDate);
+                })
+                console.log(fiveDaysData);
+
+                //show data on div
+                temperature.innerHTML = Math.trunc(fiveDaysData[0].main.temp) + "°C";
+                city_name.innerHTML = result.city.name;
+                wind.innerHTML = fiveDaysData[0].wind.speed + "mi/h";
+                humidity.innerHTML = fiveDaysData[0].main.humidity + "%";
+                visibility.innerHTML = fiveDaysData[0].visibility + "mi";
+                pressure.innerHTML = fiveDaysData[0].main.pressure + "hPa";
+
+                //Applying elseif condition to change icon and background according to weather
+                if(fiveDaysData[0].weather[0].main == "Clouds") {
+                    icon.src = "weather-icons/cloud-icon.png";
+                    img_para.innerHTML = "Cloudy";
+                    img_para.style.color = "black";
+                    inside_second_div.style.backgroundImage = 'url("weather-icons/cloudy-background.jpg")';
+                    inside_second_div.style.backgroundSize = '100% 256px';
+                    inside_second_div.style.backgroundPosition = 'center';
+                    temperature.style.color = "black";
+                    city_name.style.color = "black";
+                    wind.style.color = "black";
+                    humidity.style.color = "black";
+                    visibility.style.color = "black";
+                    pressure.style.color = "black";
+
+                }
+                else if(fiveDaysData[0].weather[0].main == "Clear") {
+                    icon.src = "weather-icons/clear-icon.png";
+                    img_para.innerHTML = "Clear";
+                    img_para.style.color = "black";
+                    inside_second_div.style.backgroundImage = 'url("weather-icons/clear-background.jpg")';
+                    inside_second_div.style.backgroundSize = '100% 256px';
+                    inside_second_div.style.backgroundPosition = 'center';
+                    temperature.style.color = "black";
+                    city_name.style.color = "black";
+                    wind.style.color = "black";
+                    humidity.style.color = "black";
+                    visibility.style.color = "black";
+                    pressure.style.color = "black";
+                }
+                else if(fiveDaysData[0].weather[0].main == "Rain") {
+                    icon.src = "weather-icons/rain-icon.png";
+                    img_para.innerHTML = "Rainy";
+                    img_para.style.color = "white";
+                    inside_second_div.style.backgroundImage = 'url("weather-icons/rain-background.jpg")';
+                    inside_second_div.style.backgroundSize = 'cover';
+                    inside_second_div.style.backgroundPosition = 'center';
+                    temperature.style.color = "white";
+                    city_name.style.color = "white";
+                    wind.style.color = "white";
+                    humidity.style.color = "white";
+                    visibility.style.color = "white";
+                    pressure.style.color = "white";
+                }
+                else if(fiveDaysData[0].weather[0].main == "Snow") {
+                    icon.src = "weather-icons/snow-icon.png";
+                    img_para.innerHTML = "Snow";
+                    img_para.style.color = "black";
+                    inside_second_div.style.backgroundImage = 'url("weather-icons/snow-background.jpg")';
+                    inside_second_div.style.backgroundSize = 'cover';
+                    inside_second_div.style.backgroundPosition = 'center';
+                    temperature.style.color = "black";
+                    city_name.style.color = "black";
+                    wind.style.color = "black";
+                    humidity.style.color = "black";
+                    visibility.style.color = "black";
+                    pressure.style.color = "black";
+
+                }
+                else if(fiveDaysData[0].weather[0].main == "Haze") {
+                    icon.src = "weather-icons/partly-cloudy-icon.png";
+                    img_para.innerHTML = "Partly Cloudy";
+                    img_para.style.color = "black";
+                    inside_second_div.style.backgroundImage = 'url("weather-icons/partly-cloudy-background.jpg")';
+                    inside_second_div.style.backgroundSize = 'cover';
+                    inside_second_div.style.backgroundPosition = 'center';
+                    temperature.style.color = "black";
+                    city_name.style.color = "black";
+                    wind.style.color = "black";
+                    humidity.style.color = "black";
+                    visibility.style.color = "black";
+                    pressure.style.color = "black";
+                }
+
+                //Display next five days data
+                document.querySelector(".temp1").innerHTML = "Temp:" + Math.trunc(fiveDaysData[1].main.temp) +"°C";
+                document.querySelector(".wind1").innerHTML = "Wind:" + fiveDaysData[1].wind.speed + "mi/h";
+                document.querySelector(".humidity1").innerHTML = "Humidity:" + fiveDaysData[1].main.humidity + "%";
+
+                if(fiveDaysData[1].weather[0].main == "Clouds") {
+                    document.querySelector(".next-1-img").src = "weather-icons/cloud-icon.png";
+                }
+                else if(fiveDaysData[1].weather[0].main == "Clear") {
+                    document.querySelector(".next-1-img").src = "weather-icons/clear-icon.png";
+                }
+                else if(fiveDaysData[1].weather[0].main == "Rain") {
+                    document.querySelector(".next-1-img").src = "weather-icons/rain-icon.png";
+                }
+                else if(fiveDaysData[1].weather[0].main == "Snow") {
+                    document.querySelector(".next-1-img").src = "weather-icons/snow-icon.png";
+                }
+                else if(fiveDaysData[1].weather[0].main == "Haze") {
+                    document.querySelector(".next-1-img").src = "weather-icons/partly-cloudy-icon.png";
+                }
+
+                document.querySelector(".date2").innerHTML = fiveDaysData[2].dt_txt.split(" ")[0];
+                document.querySelector(".temp2").innerHTML = "Temp:" + Math.trunc(fiveDaysData[2].main.temp) +"°C";
+                document.querySelector(".wind2").innerHTML = "Wind:" + fiveDaysData[2].wind.speed + "mi/h";
+                document.querySelector(".humidity2").innerHTML = "Humidity:" + fiveDaysData[2].main.humidity + "%";
+                if(fiveDaysData[2].weather[0].main == "Clouds") {
+                    document.querySelector(".next-2-img").src = "weather-icons/cloud-icon.png";
+                }
+                else if(fiveDaysData[2].weather[0].main == "Clear") {
+                    document.querySelector(".next-2-img").src = "weather-icons/clear-icon.png";
+                }
+                else if(fiveDaysData[2].weather[0].main == "Rain") {
+                    document.querySelector(".next-2-img").src = "weather-icons/rain-icon.png";
+                }
+                else if(fiveDaysData[2].weather[0].main == "Snow") {
+                    document.querySelector(".next-2-img").src = "weather-icons/snow-icon.png";
+                }
+                else if(fiveDaysData[2].weather[0].main == "Haze") {
+                    document.querySelector(".next-2-img").src = "weather-icons/partly-cloudy-icon.png";
+                }
+    
+                document.querySelector(".date3").innerHTML = fiveDaysData[3].dt_txt.split(" ")[0];
+                document.querySelector(".temp3").innerHTML = "Temp:" + Math.trunc(fiveDaysData[3].main.temp) +"°C";
+                document.querySelector(".wind3").innerHTML = "Wind:" + fiveDaysData[3].wind.speed + "mi/h";
+                document.querySelector(".humidity3").innerHTML = "Humidity:" + fiveDaysData[3].main.humidity + "%";
+                if(fiveDaysData[3].weather[0].main == "Clouds") {
+                    document.querySelector(".next-3-img").src = "weather-icons/cloud-icon.png";
+                }
+                else if(fiveDaysData[3].weather[0].main == "Clear") {
+                    document.querySelector(".next-3-img").src = "weather-icons/clear-icon.png";
+                }
+                else if(fiveDaysData[3].weather[0].main == "Rain") {
+                    document.querySelector(".next-3-img").src = "weather-icons/rain-icon.png";
+                }
+                else if(fiveDaysData[3].weather[0].main == "Snow") {
+                    document.querySelector(".next-3-img").src = "weather-icons/snow-icon.png";
+                }
+                else if(fiveDaysData[3].weather[0].main == "Haze") {
+                    document.querySelector(".next-3-img").src = "weather-icons/partly-cloudy-icon.png";
+                }
+     
+                document.querySelector(".date4").innerHTML = fiveDaysData[4].dt_txt.split(" ")[0];
+                document.querySelector(".temp4").innerHTML = "Temp:" + Math.trunc(fiveDaysData[4].main.temp) +"°C";
+                document.querySelector(".wind4").innerHTML = "Wind:" + fiveDaysData[4].wind.speed + "mi/h";
+                document.querySelector(".humidity4").innerHTML = "Humidity:" + fiveDaysData[4].main.humidity + "%";
+                if(fiveDaysData[4].weather[0].main == "Clouds") {
+                    document.querySelector(".next-4-img").src = "weather-icons/cloud-icon.png";
+                }
+                else if(fiveDaysData[4].weather[0].main == "Clear") {
+                    document.querySelector(".next-4-img").src = "weather-icons/clear-icon.png";
+                }
+                else if(fiveDaysData[4].weather[0].main == "Rain") {
+                    document.querySelector(".next-4-img").src = "weather-icons/rain-icon.png";
+                }
+                else if(fiveDaysData[4].weather[0].main == "Snow") {
+                    document.querySelector(".next-4-img").src = "weather-icons/snow-icon.png";
+                }
+                else if(fiveDaysData[4].weather[0].main == "Haze") {
+                    document.querySelector(".next-4-img").src = "weather-icons/partly-cloudy-icon.png";
+                }
+
+                document.querySelector(".date5").innerHTML = fiveDaysData[5].dt_txt.split(" ")[0];
+                document.querySelector(".temp5").innerHTML = "Temp:" + Math.trunc(fiveDaysData[5].main.temp) +"°C";
+                document.querySelector(".wind5").innerHTML = "Wind:" + fiveDaysData[5].wind.speed + "mi/h";
+                document.querySelector(".humidity5").innerHTML = "Humidity:" + fiveDaysData[5].main.humidity + "%";
+                if(fiveDaysData[5].weather[0].main == "Clouds") {
+                    document.querySelector(".next-5-img").src = "weather-icons/cloud-icon.png";
+                }
+                else if(fiveDaysData[5].weather[0].main == "Clear") {
+                    document.querySelector(".next-5-img").src = "weather-icons/clear-icon.png";
+                }
+                else if(fiveDaysData[5].weather[0].main == "Rain") {
+                    document.querySelector(".next-5-img").src = "weather-icons/rain-icon.png";
+                }
+                else if(fiveDaysData[5].weather[0].main == "Snow") {
+                    document.querySelector(".next-5-img").src = "weather-icons/snow-icon.png";
+                }
+                else if(fiveDaysData[5].weather[0].main == "Haze") {
+                    document.querySelector(".next-5-img").src = "weather-icons/partly-cloudy-icon.png";
+                }
+            }
+
+            current_location();
+
+        },
+        //Handling error
+        Error => {
+            console.log(Error);
+            alert(Error);
+        }
+    );
+}
+
+current_location_btn.addEventListener("click", getcity);
